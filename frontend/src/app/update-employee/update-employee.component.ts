ab5e6ff022b2ec791d96c82adfdec65521141183
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-06 09:54:08 -0300

    Connecting Angular with Get Employee REST API

--- frontend/src/app/update-employee/update-employee.component.ts
+++ frontend/src/app/update-employee/update-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 
@@ -9,9 +9,19 @@ import { EmployeeService } from '../employee.service';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
+  id: number;
+  employee: Employee;
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute, private router: Router) { }
 
-  employee: Employee = new Employee();
-  constructor(private employeeService: EmployeeService, private router: Router) { }
+  ngOnInit() {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      console.log(data);
+      this.employee = data;
+    }, error => console.log(error));
+
+  }
 
   saveEmployee() {
     this.employeeService.createEmployee(this.employee).subscribe(data => {
@@ -21,11 +31,8 @@ export class UpdateEmployeeComponent implements OnInit {
       error => console.log(error));
   }
 
-  ngOnInit(): void {
-  }
-
   goToEmployeeList() {
-    this.router.navigate(['employees']);
+    this.router.navigate(['/employees']);
   }
 
   onSubmit() {
