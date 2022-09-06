479c9aaab6384897683bd869b4ede9629e694096
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-09-06 08:48:11 -0300

    Create Angular Update Employee Component & Form

---
+++ frontend/src/app/update-employee/update-employee.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
+})
+export class UpdateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  saveEmployee() {
+    this.employeeService.createEmployee(this.employee).subscribe(data => {
+      console.log(data);
+      this.goToEmployeeList();
+    },
+      error => console.log(error));
+  }
+
+  ngOnInit(): void {
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['employees']);
+  }
+
+  onSubmit() {
+    console.log(this.employee);
+    this.saveEmployee();
+  }
+
+}
